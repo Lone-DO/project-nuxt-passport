aaa7fcbfc934aec2fbbaeea4970a57a9cdbe041f
lone-DO <citizin@outlook.com>
2025-08-29 01:22:43 +0200

    Integrate Nuxt Plugins
    - Tailwind
    - DaisyUI
    - Icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello World!
+    </h1>
+    <button class="btn btn-accent">
+      Log in with GitHub <icon name="tabler:user" />
+    </button>
   </div>
 </template>
