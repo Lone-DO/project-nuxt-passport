3afe03c567c527bfcf35a335e34c4dadc2a785e7
lone-DO <citizin@outlook.com>
2025-08-29 11:25:12 +0200

    Landing Page and Layout Setup

---
+++ app/components/app/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <nuxt-link to="/" class="btn btn-ghost text-xl ">
+        Travel Log
+      </nuxt-link>
+    </div>
+    <div class="navbar-end">
+      <AuthButton />
+    </div>
+  </header>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppHeader />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
