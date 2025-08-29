6dd252c5c1e47324a1e1c98dd7897de8f2cb61a5
lone-DO <citizin@outlook.com>
2025-08-29 17:05:22 +0200

    Update Schemas with user.id reference

--- app/lib/db/schema/location-log.ts
+++ app/lib/db/schema/location-log.ts
@@ -1,5 +1,6 @@
 import { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
+import { user } from './auth';
 import { location } from './location';
 
 export const locationLog = sqliteTable('location-log', {
@@ -10,7 +11,10 @@ export const locationLog = sqliteTable('location-log', {
   endedAt: int().notNull(),
   lat: real().notNull(),
   long: real().notNull(),
+
   locationId: int().notNull().references(() => location.id),
+  userId: int().notNull().references(() => user.id),
+
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
