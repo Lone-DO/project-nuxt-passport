120c9f4c106191d9e3d91666af281a8cfee2fec7
lone-DO <citizin@outlook.com>
2025-08-30 00:39:48 +0200

    Integrate Nuxt/Pinia Plugin && AuthStore init

--- app/components/auth/button.vue
+++ app/components/auth/button.vue
@@ -1,5 +1,21 @@
+<script lang='ts' setup>
+import { useAuthStore } from '~/stores';
+
+const authStore = useAuthStore();
+</script>
+
 <template>
-  <button class="btn btn-accent">
-    Sign in with GitHub <Icon name="uil:github" size="32" />
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign in with GitHub
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon
+      v-else
+      name="uil:github"
+      size="32"
+    />
   </button>
 </template>
