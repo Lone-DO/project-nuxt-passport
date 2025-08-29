f4716543556cad078793f62e1bc80c3c7423acf3
Dayshawn Losovoj (Citizin) <Lone-DO@users.noreply.github.com>
2025-08-29 17:10:33 +0200

    15 auth configurations (#54)
    * @better-auth/cli generate
    
    * Update Schemas with user.id reference
    
    * DB Migration
    
    * Update ENV

--- app/lib/db/schema/location-log-image.ts
+++ app/lib/db/schema/location-log-image.ts
@@ -1,11 +1,15 @@
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
+import { user } from './auth';
 import { locationLog } from './location-log';
 
 export const locationLogImage = sqliteTable('location-log-image', {
   id: int().primaryKey({ autoIncrement: true }),
   key: text().notNull(),
+
   locationId: int().notNull().references(() => locationLog.id),
+  userId: int().notNull().references(() => user.id),
+
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
