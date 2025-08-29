c7fb754d32d6c765866d52d8080f4aff5cb4232a
Dayshawn Losovoj (Citizin) <Lone-DO@users.noreply.github.com>
2025-08-29 11:30:03 +0200

    Landing Page and Layout Setup (#50)

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-    <button class="btn btn-accent">
-      Log in with GitHub <icon name="tabler:user" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/components/auth/button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="btn btn-accent">
+    Sign in with GitHub <Icon name="uil:github" size="32" />
+  </button>
+</template>
