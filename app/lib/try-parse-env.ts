83ffead2046b18c09b682be9d6fd3674cd6c19f9
lone-DO <citizin@outlook.com>
2025-08-29 12:36:43 +0200

    Env Schema Validation

---
+++ app/lib/try-parse-env.ts
@@ -0,0 +1,29 @@
+/** https://github.com/w3cj/nuxt-travel-log/pull/51/commits/33af03ac274544851b9652319ec0d257cbab8449 */
+
+/* eslint-disable node/no-process-env */
+import type { ZodObject, ZodRawShape } from 'zod';
+
+import { ZodError } from 'zod';
+/** Pretties ZOD Errors after validating .env keys */
+export default function tryParseEnv<T extends ZodRawShape>(
+  EnvSchema: ZodObject<T>,
+  buildEnv: Record<string, string | undefined> = process.env,
+) {
+  try {
+    EnvSchema.parse(buildEnv);
+  }
+  catch (error) {
+    if (error instanceof ZodError) {
+      let message = 'Missing required values in .env:\n';
+      error.issues.forEach((issue) => {
+        message += `${String(issue.path[0])}\n`;
+      });
+      const e = new Error(message);
+      e.stack = '';
+      throw e;
+    }
+    else {
+      console.error(error);
+    }
+  }
+}
