394b706c5627aa82e4f6fc82dd9381c9493ae8ec
Dayshawn Losovoj (Citizin) <Lone-DO@users.noreply.github.com>
2025-09-01 14:12:21 +0200

    21 - dashboard navigation (#60)
    * Dashboard Navigation Init
    
    * Inject Dashboard Navigation

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import Navigation from './navigation.vue';
+</script>
+
 <template>
-  <div>
-    <span>DASHBOARD</span>
-  </div>
+  <section class="flex flex-1">
+    <Navigation />
+    <div class="flex-1">
+      <span>Dashboard</span>
+    </div>
+  </section>
 </template>
