b6cc297fdfd0c3f0701f14f63da558315a5ed607
lone-DO <citizin@outlook.com>
2025-09-02 21:12:50 +0200

    Move Dashboard Root Page

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="dashboard" class="flex">
+    <DashboardNavigation />
+    <div class="flex-1 flex justify-center">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
