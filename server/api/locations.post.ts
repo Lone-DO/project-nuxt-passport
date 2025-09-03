75a4ff9e0973360ef9210b7d9dbb69a5f1d13539
Dayshawn Losovoj (Citizin) <Lone-DO@users.noreply.github.com>
2025-09-03 14:34:41 +0200

    24 - DB Query/CRUD Validations (#62)
    * DB Validation Helpers
    Add global helpers that validate flows,
    - WHEN valid, returns true or parsed data
    - WHEN invalid, throws custom error
    
    * DB Query Helpers
    Add global query helpers
    
    * Inject nanoid && Update location POST to use helpers
    
    * Dashboard/new add fallback error capture for PROD env

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,52 +1,20 @@
-import { auth } from '~/lib/auth';
-import db from '~/lib/db';
-import { InsertLocation, location } from '~/lib/db/schema';
+import { findUniqueSlug, injectLocation } from '~/lib/db/queries';
+import { validateLocationPayload, validateUniqueLocationName, validateUserSession } from '~/lib/db/validators';
 
 export default defineEventHandler(async (event) => {
   /**
    * Fetch User Session from AuthClient
    * https://www.better-auth.com/docs/concepts/session-management#get-session
    */
-  const session = await auth.api.getSession({
-    headers: event.headers,
-  });
-  /** IF User is NOT logged in, throw unauth error */
-  if (!session?.user) {
-    return sendError(event, createError({
-      statusCode: 401,
-      statusMessage: 'Unauthorized',
-    }));
-  }
-  /** Validate submitted Data matches location SCHEMA */
-  const result = await readValidatedBody(event, InsertLocation.safeParse);
-  /** IF submitted Data is invalid, return errors to form */
-  if (!result.success) {
-    /** Pretty Errors messages as Key/value pairs */
-    let statusMessage: string[] | string = [];
-    const data: Record<string, string> = {};
-    for (const issue of result.error.issues) {
-      /** expected example("name: required") */
-      statusMessage.push(`${issue.path.join('')}: ${issue.message}`);
-      data[issue.path.join('')] = issue.message;
-    }
-    statusMessage = statusMessage.join('; ');
-
-    return sendError(event, createError({
-      statusCode: 422,
-      statusMessage,
-      data,
-    }));
-  }
+  const session = await validateUserSession(event);
+  const result = await validateLocationPayload(event);
+  await validateUniqueLocationName(result.name, Number(session.user.id));
+  /** Verify that location.slug is UNIQUE */
+  const slug = await findUniqueSlug(result.name);
 
   /**
    * Capture created data instance and return to client
    * TODO: After creating instance, redirect user to location details (/dashboard/:locationID)
    */;
-  const [created] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(' ', '-').toLowerCase(),
-    userId: session.user.id as unknown as number,
-  }).returning();
-
-  return created;
+  return injectLocation(result, slug, (session.user.id as unknown as number));
 });
