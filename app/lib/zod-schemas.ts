19752bbd8e86c302e7b37680e6483918acf4c0e2
lone-DO <citizin@outlook.com>
2025-09-03 10:58:56 +0200

    TODO ZOD 4 Migration

--- app/lib/zod-schemas.ts
+++ app/lib/zod-schemas.ts
@@ -6,6 +6,10 @@ export const SearchSchema = z.object({
 
 export type SearchSchema = z.infer<typeof SearchSchema>;
 
+/**
+ * TODO: Update to ZOD 4
+ * https://zod.dev/v4/changelog (Migration guide)
+ */
 export const NameSchema = z.string().min(1).max(100);
 export const DescriptionSchema = z.string().max(1000).or(z.null());
 export const LatSchema = z.coerce.number().min(-90).max(90);
