13ae12521e92cf4c7c11030cc59e695ef108ad39
lone-DO <citizin@outlook.com>
2025-09-07 03:24:01 +0200

    Move DashboardLocationItem

---
+++ app/components/location/item.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+import type { location } from '~/lib/db/queries';
+
+defineProps<{
+  location: location;
+}>();
+
+const mapStore = useMapStore();
+</script>
+
+<template>
+  <article
+    class="dashboard-location-item card-body cursor-pointer"
+    @mouseenter="mapStore.syncPin(location, true)"
+    @mouseleave="mapStore.syncPin(location, false)"
+  >
+    <h2 class="card-title">
+      {{ location?.name }}
+    </h2>
+    <p>{{ location?.description || '...' }}</p>
+  </article>
+</template>
