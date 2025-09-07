8581945e2753ad66a63ee9a8cdbf8e02d8bb5f36
Dayshawn Losovoj (Citizin) <Lone-DO@users.noreply.github.com>
2025-09-07 18:15:42 +0200

    30-location-form-set-coords (#70)
    * Create NewPin for controlling coordinates,
    
    * Create Coordinates Field for Rendering selected Coords from Map

---
+++ app/components/map/new-pin.vue
@@ -0,0 +1,30 @@
+<script lang='ts' setup>
+import type { LngLatLike } from 'maplibre-gl';
+
+import { CENTER_GERMANY } from '~/lib/constants';
+
+const mapStore = useMapStore();
+const coordinates = computed<LngLatLike>(() => {
+  return !mapStore.newPin ? CENTER_GERMANY : [mapStore.newPin.long, mapStore.newPin.lat];
+});
+</script>
+
+<template>
+  <MglMarker
+    :coordinates
+    :draggable="true"
+    @update:coordinates="mapStore.syncNewPinCoords"
+  >
+    <template #marker>
+      <div
+        class="tooltip tooltip-top tooltip-open text-warning"
+        data-tip="Drag to your desired location"
+      >
+        <Icon
+          size="40"
+          name="majesticons:map-marker-area"
+        />
+      </div>
+    </template>
+  </MglMarker>
+</template>
