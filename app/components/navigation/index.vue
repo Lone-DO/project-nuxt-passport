652a78971c2efb13076a9ac9f81cc9e53954dbeb
lone-DO <citizin@outlook.com>
2025-09-09 00:06:18 +0200

    Restructure Navigation elements

--- app/components/navigation/index.vue
+++ app/components/navigation/index.vue
@@ -13,50 +13,47 @@ onMounted(() => {
 
 <template>
   <aside
-    class="dashboard-navigation p-1 flex flex-col border-r-2 border-(--custom-divider-color) transition-all duration-200"
+    class="dashboard-navigation p-1 flex flex-col border-r-2 border-(--custom-divider-color) transition-all duration-200 gap-1"
     :class="{ 'w-16': collapsed, 'w-40': !collapsed }"
   >
     <NavigationToggle v-model="collapsed" />
-    <nav>
-      <ul class="flex flex-col gap-1">
-        <li>
-          <NavigationButton
-            :name="icons.map"
-            label="Locations"
-            href="/dashboard"
-            :show-label="!collapsed"
-          />
-        </li>
-        <li>
-          <NavigationButton
-            :name="icons.add"
-            label="Add Location"
-            href="/dashboard/new"
-            :show-label="!collapsed"
-          />
-        </li>
-        <client-only>
-          <li v-if="navStore.loading || navStore.items.length" class="divider" />
-          <li v-if="navStore.loading" class="skeleton h-10 w-full" />
-          <template v-else-if="navStore.items.length">
-            <NavigationLocationItem
-              v-for="location in navStore.items"
-              :key="location.id"
-              :location
-              :collapsed
-            />
-          </template>
-          <li class="divider" />
-          <li>
-            <NavigationButton
-              :name="icons.logout"
-              label="Sign Out"
-              href="/signout"
-              :show-label="!collapsed"
-            />
-          </li>
-        </client-only>
-      </ul>
+    <nav class="flex flex-col gap-1">
+      <NavigationButton
+        :name="icons.map"
+        label="Locations"
+        href="/dashboard"
+        :show-label="!collapsed"
+      />
+      <NavigationButton
+        :name="icons.add"
+        label="Add Location"
+        href="/dashboard/new"
+        :show-label="!collapsed"
+      />
+      <template v-if="navStore.loading || navStore.items.length">
+        <li class="divider" />
+        <!-- TODO: Add Mobile/Tablet Max Height adjustments -->
+        <ul class="flex flex-col gap-1 ">
+          <client-only>
+            <li v-if="navStore.loading" class="skeleton h-10 w-full" />
+            <template v-else-if="navStore.items.length">
+              <NavigationLocationItem
+                v-for="location in navStore.items"
+                :key="location.id"
+                :location
+                :collapsed
+              />
+            </template>
+          </client-only>
+        </ul>
+      </template>
+      <div class="divider" />
+      <NavigationButton
+        :name="icons.logout"
+        label="Sign Out"
+        href="/signout"
+        :show-label="!collapsed"
+      />
     </nav>
   </aside>
 </template>
