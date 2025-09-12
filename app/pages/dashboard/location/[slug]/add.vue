e905c5827113acdcc897739d70434e334f6b0864
lone-DO <citizin@outlook.com>
2025-09-12 02:35:54 +0200

    LocationLog Form Init

---
+++ app/pages/dashboard/location/[slug]/add.vue
@@ -0,0 +1,83 @@
+<script lang='ts' setup>
+import type { NominatimResult } from '~/lib/types';
+
+import { CENTER_GERMANY, EDITING_ROUTES, NEW_FORM_LOCATION_LOG_FIELDS } from '~/lib/constants';
+import { InsertLocationLog } from '~/lib/db/schema';
+
+const [long, lat] = CENTER_GERMANY as [number, number];
+
+const { $csrfFetch } = useNuxtApp();
+const mapStore = useMapStore();
+const appStore = useAppStore();
+const locationStore = useLocationStore();
+const isReady = ref(false);
+
+const backLink = computed(() => ({ name: 'dashboard-location-slug', params: { slug: locationStore.currentItem?.slug } }));
+
+function onSubmit(values: InsertLocationLog) {
+  return $csrfFetch(`/api/locations/${mapStore.currentSlug}/add`, {
+    method: 'post',
+    body: values,
+  });
+}
+function onSubmitted() {
+  /** TODO: After submitting, redirect user to newly created item page (/dashboard/:locationID) */
+  navigateTo(backLink.value);
+}
+function syncSelectedLocation(result: NominatimResult) {
+  mapStore.newPin = {
+    id: 0,
+    name: 'Added Point',
+    description: '',
+    long: Number(result.lon),
+    lat: Number(result.lat),
+    centerMap: true,
+    zoom: 12,
+  };
+}
+/** TODO: Centralize Logic as reusable */
+onBeforeRouteLeave((to) => {
+  if (!EDITING_ROUTES.has(to.name as string)) {
+    /** onDestroy, remove global instance */
+    mapStore.newPin = null;
+  }
+});
+onMounted(() => {
+  /** onMount, stage global instance */
+  mapStore.newPin = {
+    id: 0,
+    name: '',
+    description: '',
+    long: locationStore.currentItem?.long || long,
+    lat: locationStore.currentItem?.lat || lat,
+    startedAt: new Date().getTime(),
+    endedAt: new Date().getTime(),
+    centerMap: true,
+    zoom: 12,
+  };
+  isReady.value = true;
+});
+</script>
+
+<template>
+  <article id="dashboard-location-log-new">
+    <h4 class="mb-4">
+      Add Location Log
+    </h4>
+    <p class="text-sm">
+      Logs are experiences you had while visiting the target location: {{ locationStore.currentItem?.name }}
+    </p>
+    <LocationFormBase
+      v-if="isReady"
+      :value="mapStore.newPin"
+      :callback="onSubmit"
+      :cancel-to="backLink"
+      :fields="NEW_FORM_LOCATION_LOG_FIELDS"
+      :schema="InsertLocationLog"
+      submit-label="Add"
+      :submit-icon="appStore.icons.addPath"
+      @select="syncSelectedLocation"
+      @submitted="onSubmitted"
+    />
+  </article>
+</template>
