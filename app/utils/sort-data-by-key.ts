bdc2e2d74d0edf4451759458825864077770ffbc
lone-DO <citizin@outlook.com>
2025-09-13 20:58:11 +0200

    Critical Refactors to Location & MapStores
    - Injecting currentSlug flow
    - updating zoom option to be based on Read/Write routes
    - Create Unpacking helpers

---
+++ app/utils/sort-data-by-key.ts
@@ -0,0 +1,19 @@
+export function sortDataByKey(key: string, data: { [key: string]: any }[], direction: 'asc' | 'desc' = 'asc') {
+  if (!key || !data?.length) {
+    return null;
+  }
+
+  return data?.sort((a, b) => {
+    const isAscending = direction === 'asc';
+    if (a?.[key].localeCompare) {
+      return (isAscending) ? a[key].localeCompare(b?.[key]) : b[key].localeCompare(a[key]);
+    }
+    if (a?.[key] < b?.[key]) {
+      return isAscending ? 1 : -1;
+    }
+    if (b?.[key] < a?.[key]) {
+      return isAscending ? -1 : 1;
+    }
+    return 0;
+  });
+}
