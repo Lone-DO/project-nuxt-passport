2d56ab12f340a9653d416168f139afe4820eda07
lone-DO <citizin@outlook.com>
2025-09-14 12:17:12 +0200

    Inject new LocationDetails flow

--- app/pages/dashboard/location/[slug]/logs/[id].vue
+++ app/pages/dashboard/location/[slug]/logs/[id].vue
@@ -1,86 +1,57 @@
 <script lang='ts' setup>
-import type { FetchError } from 'ofetch';
-
-import type { DropdownItem } from '~/lib/types';
-
-import getFetchErrorMessage from '~/utils/get-fetch-error';
+import { EDITING_ROUTES } from '~/lib/constants';
 
 /** Stores */
 const $route = useRoute();
-const appStore = useAppStore();
 const locationStore = useLocationStore();
 const { currentItem, currentLog: item, currentLogError: error, currentLogStatus: status } = storeToRefs(locationStore);
 /** General */
-const isDeleting = ref(false);
-const deleteError = ref<string | null>('');
-const isLoading = computed(() => status.value === 'pending' || isDeleting.value);
-const errorMessage = computed(() => error?.value?.statusMessage || deleteError.value);
-const isNestedRoute = computed(() => String($route.name).includes('dashboard-location-slug-logId-'));
-/** Modal */
-const isModalOpen = ref(false);
-const dropdownItems: DropdownItem[] = [{
-  icon: appStore.icons.delete,
-  label: 'Delete',
-  onClick: () => (isModalOpen.value = true),
-}];
-/** Form */
-const deleted = ref(false);
-const { handleSubmit } = useForm();
+const isNestedRoute = computed(() => String($route.name).includes('dashboard-location-slug-logs-id-'));
+const handlers = computed(() => {
+  if (EDITING_ROUTES.has($route.name as string)) {
+    return { submitted: locationStore.refreshCurrentItem };
+  }
+  return {};
+});
+
 watch(() => locationStore.locationUrlWithId, () => {
   /** onRouteParams change, refresh current location */
   locationStore.refreshCurrentLog();
 }, { immediate: true });
-const onDelete = handleSubmit(async () => {
-  try {
-    deleteError.value = null;
-    isDeleting.value = true;
-    await $fetch(`/api/locations/${currentItem.value?.slug}/logs/${item.value?.id}`, {
-      method: 'DELETE',
-    });
-    deleted.value = true;
-    navigateTo({
-      name: 'dashboard-location-slug',
-      params: { params: currentItem.value?.slug },
-    });
-  }
-  catch (_error) {
-    const error = _error as FetchError;
-    /** error.data.statusMessage is fallback for PROD captures */
-    deleteError.value = getFetchErrorMessage(error);
-  }
-  finally {
-    isDeleting.value = false;
-  }
-});
+
+function onDeleted() {
+  navigateTo({
+    name: 'dashboard-location-slug',
+    params: { params: currentItem.value?.slug },
+  });
+}
+const deleteUrl = computed(() => `/api/locations/${currentItem.value?.slug}/logs/${item.value?.id}`);
+const modalTitle = computed(() => `Delete Location Log: ${item.value?.name}`);
+const modalDescription = 'Deleting this location log will also delete all associated data';
 </script>
 
 <template>
-  <article id="dashboard-location-slug-logId" class="flex flex-col gap-2">
-    <span v-if="isLoading" class="loading loading-spinner loading-md" />
-    <span
-      v-if="!isLoading && errorMessage"
-      role="alert"
-      class="alert alert-error justify-center"
-    > {{ errorMessage }}</span>
-    <template v-if="!isLoading && item && !isNestedRoute">
-      <LocationDeleteDialog
-        :location="item"
-        :is-open="isModalOpen"
-        @accept="onDelete"
-        @on-closed="isModalOpen = false"
-      />
-      <h4 class="mb-3">
-        Location: {{ item.name }} <AppDropdown :items="dropdownItems" />
-      </h4>
-      <p class="text-sm">
-        Description: {{ item.description || '--' }}
-      </p>
-      <p class="text-sm">
-        Started At: {{ new Date(item?.startedAt).toLocaleDateString() }}
-      </p>
-      <p class="text-sm">
-        Ended At: {{ new Date(item?.endedAt).toLocaleDateString() }}
-      </p>
-    </template>
-  </article>
+  <div id="location-log-id">
+    <LocationDetails
+      v-if="!isNestedRoute"
+      :item
+      :error
+      :status
+      :delete-url
+      :modal-title
+      :modal-description
+      type="locationLog"
+      @deleted="onDeleted"
+    >
+      <template v-if="item">
+        <p class="text-sm">
+          Started At: {{ new Date(item?.startedAt).toLocaleDateString() }}
+        </p>
+        <p class="text-sm">
+          Ended At: {{ new Date(item?.endedAt).toLocaleDateString() }}
+        </p>
+      </template>
+    </LocationDetails>
+    <NuxtPage v-else v-on="handlers" />
+  </div>
 </template>
